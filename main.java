2ec1d14e80f7df50f92d18a7e9a6215c08960fd9
Albert <albertdundych@gmail.com>
2026-03-24 20:47:55 +0100

    update

--- main.java
+++ main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Car myCar = new Car (50000, "red", "Tesla", 500, "electric");
         myCar.drive();
+
+        Mobile myMobile = new Mobile (2499.99, true, "samsung", "black", 100000);
     }
 }
